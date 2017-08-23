5e279046481c3f2ed45de523f1a072ffbc4d5885
snake5 <snake5creator@gmail.com>
2017-08-23 10:08:52 +0300

    windows export done

--- bin/build.py
+++ bin/build.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
-import argparse, os, platform, shutil, subprocess
+import argparse, os, platform, shutil, subprocess, glob
+
+
+def safe_delete( dp ):
+	if os.path.exists( dp ):
+		os.rename( dp, dp + "~" )
+		shutil.rmtree( dp + "~" )
+	os.mkdir( dp )
+def path_under( testpath, parentpath ):
+	testpath = os.path.realpath( testpath )
+	parentpath = os.path.realpath( parentpath )
+	return os.path.commonprefix([ testpath, parentpath ]) == parentpath
 
 RESOURCE_PATH = "Resources"
 
@@ -12,25 +23,40 @@ parser.add_argument( "-e", "--exeprefix", required = True )
 parser.add_argument( "-p", "--platform", required = True, choices = [ "windows" ] )
 args = parser.parse_args()
 
+DEVDIR = os.path.dirname( os.path.realpath( __file__ ) )
+TGTDIR = args.dir
+def DEVP( *aa ):
+	return os.path.join( DEVDIR, *aa )
+def TGTP( *aa ):
+	return os.path.join( TGTDIR, *aa )
+if path_under( TGTDIR, DEVDIR ):
+	raise Exception( "cannot export to development directory" )
 
-def safe_delete( dp ):
-	if os.path.exists( dp ):
-		os.rename( dp, dp + "~" )
-		shutil.rmtree( dp + "~" )
-	os.mkdir( dp )
 
 def build_windows():
-	safe_delete( args.dir )
+	safe_delete( TGTDIR )
 	# binaries
-	shutil.copy( "sgsouzel-win32.exe", os.path.join( args.dir, args.exeprefix + "-win32.exe" ) )
+	shutil.copy(
+		DEVP( "sgsouzel-win32.exe" ),
+		os.path.join( TGTDIR, args.exeprefix + "-win32.exe" ) )
 	# settings
-	if os.path.exists( "settings-production.ini" ):
-		shutil.copy( "settings-production.ini", args.dir )
-	else
-		shutil.copy( "settings.ini", args.dir )
+	if os.path.exists( DEVP( "settings-production.ini" ) ):
+		shutil.copy( DEVP( "settings-production.ini" ), TGTDIR )
+	else:
+		shutil.copy( DEVP( "settings.ini" ), TGTDIR )
 	# scripts
+	scripts = [ os.path.relpath( ff, DEVDIR )
+		for f in os.walk( DEVDIR )
+		for ff in glob.glob( os.path.join( f[0], "*.sgs" ) )
+	]
+	print( scripts )
+	for script in scripts:
+		dirpath = TGTP( os.path.dirname( script ) )
+		if not os.path.exists( dirpath ):
+			os.makedirs( dirpath )
+		shutil.copy( DEVP( script ), TGTP( script ) )
 	# resources
-	shutil.copytree( RESOURCE_PATH, os.path.join( args.dir, RESOURCE_PATH ) )
+	shutil.copytree( DEVP( RESOURCE_PATH ), TGTP( RESOURCE_PATH ) )
 
 
 if args.platform == "windows":
